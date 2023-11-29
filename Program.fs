ec04958ed9eee0b5c93a61f8ab62b4041724848a
HamenjeetKaur <HamenjeetKaur@triosstudent.com>
2023-11-29 09:23:09 -0500

    v1

---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+// Part A
+
+type gen =
+|Horror 
+|Drama
+|Thriller
+|Comedy
+|Fantasy
+|Sport
+
+type dir={
+    Name: string
+    Movies: int
+}
+
+type movie={
+    Name: string
+    runlen: int
+    Genre: gen
+    Director: dir
+    IMBD: float
+}
+
+
